463623221ae6d8df739d5ec823e6ca7375637291
Your Name Nontaphat003 <s6506021620083@email.kmutnb.ac.th>
2024-02-13 09:31:22 +0700

    7

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express()
 var bodyParser = require('body-parser')
 
 
-const base_url = "http://localhost:3000"
-//const base_url = "http://noderest0.proen.app.ruk-com.cloud/"
+//const base_url = "http://localhost:3000"
+const base_url = "http://noderest0.proen.app.ruk-com.cloud/"
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
